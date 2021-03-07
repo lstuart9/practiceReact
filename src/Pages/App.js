c3c71b86068d30eb73b4ef9c3451cbea62637fcf
lstuart9 <67130942+lstuart9@users.noreply.github.com>
2021-03-07 15:25:41 -0500

    refactoring for chris

---
+++ src/Pages/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import heart from '../Components/heart.jpg'
+
+
+export default() =>  (
+    <div >
+
+        <div className="topnav">
+            <a className="active" href="#These">These</a>
+            <a href="#Dont">Dont</a>
+            <a href="#Go">Go</a>
+            <a href="#Anywhere">Anywhere</a>
+        </div>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "lightblue"}}>
+                <h1>Howdy there don't break</h1>
+                <img src={heart} alt={"heart"} style={{width: 150, height: 100}}/>
+                <h2>This is an example body paragraph, the rest will be filled with gibberish
+                        kdsnfjjdshfndsjnfjnsdnfndsjfnjksdjfsaf
+                        sdfsdfsfsdaffsdfhgisdhfjkdjshfkjsdfdsbfdsbndslfnpnsdkfhsaj
+                        njkdbfjkasbfnmksdlanfmkdslanfm,sl
+                        hjiwohrjeopjakfophjkelpgjnkelpw</h2>
+            </div>
+        </div>
+)
+
+
